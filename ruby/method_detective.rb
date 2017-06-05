6f741a75256e7e6cfc1473963c961715f7b4e895
yichongw <yichongw@yahoo.com>
2017-06-04 19:30:29 -0700

    Replace in the <???> with the appropriate method

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,42 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+puts "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+puts "zom".insert(1, 'o')
+puts "zom".insert(2, 'o')
 # => “zoom”
 
-# "enhance".<???>
+puts "enhance".center("enhance".length+8)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+puts "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+puts "the usual".insert(-1, ' suspects')
+puts "the usual".concat(" suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+puts " suspects".insert(0, 'the usual')
+puts " suspects".prepend("the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+puts "The case of the disappearing last letter"[0..-2]
+puts "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+puts "The mystery of the missing first letter"[1..-1]
+puts "The mystery of the missing first letter".delete("T")
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+puts "Elementary,    my   dear        Watson!".squeeze(' ').strip
+puts "Elementary,    my   dear        Watson!".gsub(/\s+/, " ").strip
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+puts "z".getbyte(0)
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+puts "How many times does the letter 'a' appear in this string?".count("a")
 # => 4
