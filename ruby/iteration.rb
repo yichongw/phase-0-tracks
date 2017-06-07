9beb190b96c7fee094d099b5464d1833b08e6b30
yichongw <yichongw@yahoo.com>
2017-06-06 18:47:41 -0700

    Add Release 2 to file

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,11 +1,11 @@
 # Release 0:
-# def greeting
-# 	name = "John"
-# 	puts "Hi!"
-# 	yield (name)
-# end
+def greeting
+	name = "John"
+	puts "Hi!"
+	yield (name)
+end
 
-# greeting {|name| puts "How are you, #{name}?"}
+greeting {|name| puts "How are you, #{name}?"}
 
 # Release 1:
 people = ["sarah", "Jeff", "mike", "rachael", "Gloria", "Pat"]
@@ -32,3 +32,44 @@ people.map! do |y|
 end
 p people
 
+# Release 2:
+numbers = [2, 8, 13, 22, 15, 16, 17]
+# 1
+numbers.each {|x| p numbers.delete(x) if x % 3 == 0}
+p numbers
+
+# 2
+numbers.keep_if {|x| x < 5}
+p numbers
+
+# 3
+numbers.select! {|x| x % 5 == 0}
+p numbers
+
+# 4
+number = numbers.take_while {|x| x < 16}
+p number
+
+alphebet = {
+	a: 1,
+	b: 12,
+	c: 6,
+	d: 29,
+	e: 71
+}
+
+#1
+alphebet_del = alphebet.delete_if {|k, v| v.even?}
+p alphebet_del
+
+#2
+alphebet_fil = alphebet.select {|k, v| k > :b}
+p alphebet_fil
+
+#3
+alphebet_keep = alphebet.keep_if {|k, v| v % 6 == 0}
+p alphebet_keep
+
+# 4
+alphebet_remove = alphebet.each {|k, v| alphebet.delete(k) if v < 20} 
+p alphebet_remove
