78efebed2f76a70976c621dee55e01ddcbf407fd
yichongw <yichongw@yahoo.com>
2017-06-06 18:04:09 -0700

    Add Release 1 to file

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,9 +1,34 @@
-def greeting
-	name = "John"
-	puts "Hi!"
-	yield (name)
+# Release 0:
+# def greeting
+# 	name = "John"
+# 	puts "Hi!"
+# 	yield (name)
+# end
+
+# greeting {|name| puts "How are you, #{name}?"}
+
+# Release 1:
+people = ["sarah", "Jeff", "mike", "rachael", "Gloria", "Pat"]
+
+food = {
+	tools: ["knife", "cutting board", "napkins", "plate", "pam"],
+	ingredients: ["tomato", "lettuce", "bacon", "salt", "pepper", "cheese"]
+}
+
+p food
+food.each {|x| p x}
+
+p food[:tools]
+food[:tools].map! do |x| 
+	puts x
+	x.upcase	
 end
+p food[:tools]
 
-greeting {|name| puts "How are you, #{name}?"}
-
+p people
+people.map! do |y| 
+	puts y
+	y.capitalize
+end
+p people
 
