5914b4ba69410fc087236e4846a72278d2ed0100
yichongw <yichongw@yahoo.com>
2017-06-11 01:10:59 -0700

    Add Release 0

---
+++ ruby/puppy.rb
@@ -0,0 +1,20 @@
+# Release 0
+# SPECIES ------------------------
+# Puppy
+
+# CHARACTERISTICS ----------------
+# Eyelash length: varies
+# Friendly: yes
+# Eye count: 2
+# Name: varies
+# Color: varies
+
+# BEHAVIOR -----------------------
+# Jump
+# Run
+# Walk
+# Lick
+# Bark
+# Eat
+# Drink
+
