a1f4be2b94c748d4743b9ee89cec08aadb10932b
yichongw <yichongw@yahoo.com>
2017-06-13 00:04:22 -0700

    Add Release 1 to file

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,3 @@
-# Release 0:
 class Santa
 
 def speak
@@ -9,12 +8,32 @@ def eat_milk_and_cookies(cookie)
 	p "That was a good #{cookie}!"
 end
 
-def initialize
+def initialize(gender, ethnicity)
 	p "Initializing Santa instance ..."
-end
+	@gender = gender
+	@ethnicity = ethnicity
+	@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+	@age = 0
 end
 
-xmas = Santa.new
+def race(ethnicity)
+	puts "Santa is #{ethnicity}."
+end
 
-xmas.speak
-xmas.eat_milk_and_cookies("Oreo")
+def age
+	p @age
+end
+# xmas = Santa.new
+
+# xmas.speak
+# xmas.eat_milk_and_cookies("Oreo")
+
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
+
+# p xmas.age
+# p xmas.ethnicity = Asian
