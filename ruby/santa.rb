3a8d7c91ef4195ec3d933d4eb47c18d03c0563a7
yichongw <yichongw@yahoo.com>
2017-06-13 22:36:50 -0700

    Add multiple Santas with different ages, genders, and ethnicities

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,32 +1,56 @@
 class Santa
+	attr_reader :age, :ethnicity
+	attr_accessor :gender
 
-def speak
-	p "Ho, ho, ho! Haaaappy holidays!"
+	def initialize(gender, ethnicity, age)
+		p "Initializing Santa instance ..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = age
+	end
+
+	def speak
+		p "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie)
+		p "That was a good #{cookie}!"
+	end
+
+	def celebrate_birthday(age)
+		@age += 1
+	end
+
+	def get_mad_at(reindeer)
+		@reindeer_ranking.delete(reindeer)
+		@reindeer << reindeer
+	end
+
 end
 
-def eat_milk_and_cookies(cookie)
-	p "That was a good #{cookie}!"
+def create_santas(number)
+    santa_gender = ["Male", "Female", "Transgender", "Agender", "Bigender", "Cisgender", "Pangender", "Transsexual", "Gender Fluid", "N/A"]
+    santa_ethnicity = ["Black", "Latino", "White", "Asian", "Middle Eastern", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+    number.times {|i| p Santa.new(santa_gender.sample, santa_ethnicity.sample, rand(140))}
 end
 
-def initialize(gender, ethnicity)
-	p "Initializing Santa instance ..."
-	@gender = gender
-	@ethnicity = ethnicity
-	@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-	@age = 0
-end
+create_santas(1000)
 
-def race(ethnicity)
-	puts "Santa is #{ethnicity}."
-end
-
-def age
-	p @age
-end
 # xmas = Santa.new
 
 # xmas.speak
 # xmas.eat_milk_and_cookies("Oreo")
+
+# santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
 
 # santas = []
 # example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
