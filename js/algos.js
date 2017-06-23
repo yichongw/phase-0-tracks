8e42c197ce9a73094684c8f2f22f6dd8a1965171
yichongw <yichongw@yahoo.com>
2017-06-22 22:36:12 -0700

    Add pseudocode for method to return the longest phrase

---
+++ js/algos.js
@@ -0,0 +1,8 @@
+// Release 0:
+// pseudocode
+
+// split the array into individual strings
+// use the length method to select the string that has the longest length
+// return the string with the longest length
+// test with driver code
+
