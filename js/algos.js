1bad0292df42fe330cbd57aa5152235a15445e23
yichongw <yichongw@yahoo.com>
2017-06-23 00:49:31 -0700

    Add pseudocode, method, and driver code for release 1 (method to find a matched key value in two objects)

--- js/algos.js
+++ js/algos.js
@@ -17,5 +17,29 @@ function longestPhrase(phraseArray) {
 	console.log(phrase);
 }
 
+// Driver code:
 var getPhrase = new longestPhrase(["long phrase","longest phrase","longer phrase"]);
+
+// Release 1:
+// pseudocode
+
+// check key in first object, then check key in second object
+// if both keys match, return ture
+
+function keyPair(objectOne, objectTwo) {
+	for (var x in objectOne) {
+		for (var y in objectTwo) {
+			if (x == y && objectOne[x] == objectTwo[y]) {
+				return true;
+			}
+		}
+	} 
+	return false;
+}
+
+// // Driver code:
+keyPair({name: "Steven", age: 54}, {name: "Tamir", age: 54});
+keyPair({animal: "Dog", legs: 4}, {animal: "Cat", legs: 3});
+
+
 
