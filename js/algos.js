2159ac9d14abf8d3ff85f189b66dd05ed354f8e2
yichongw <yichongw@yahoo.com>
2017-06-22 23:42:10 -0700

    Add method that will return the longest phrase and driver code

--- js/algos.js
+++ js/algos.js
@@ -1,8 +1,21 @@
 // Release 0:
 // pseudocode
 
-// split the array into individual strings
 // use the length method to select the string that has the longest length
 // return the string with the longest length
 // test with driver code
+
+function longestPhrase(phraseArray) {
+	var longest = 0;
+	var phrase = "";
+	for (var i = 0; i < phraseArray.length; i++) {
+		if (phraseArray[i].length > longest) {
+			var longest = phraseArray[i].length;
+			phrase = phraseArray[i];
+		}
+	}
+	console.log(phrase);
+}
+
+var getPhrase = new longestPhrase(["long phrase","longest phrase","longer phrase"]);
 
